bcd94a08f600209599ecf2d4c3d9faaf700963d5
jamie crisman <jamie@awkwardhero.com>
2015-01-17 17:52:56 -0600

    basic timer functionality

--- app/entryScreen/entryScreen.js
+++ app/entryScreen/entryScreen.js
@@ -1,3 +1,5 @@
+var moment = require('moment');
+
 angular.module('entryScreen', [])
   .directive('entryScreen', function() {
     return {
@@ -6,9 +8,21 @@ angular.module('entryScreen', [])
   			$scope.activity = {
       			name: "",
       			duration: 0,
-      			notes: ""
+      			notes: "",
+      			date: moment(),
+      			timerActive: false,
+      			startTime: null,
+      			endTime: null
       		};
-      		$scope.test = 'hello world from test directive';
+      		$scope.startActivity = function(){
+      			
+      			$scope.activity.startTime = moment();
+      			$scope.activity.timerActive = true;
+      		}
+      		$scope.endActivity = function(){
+      			$scope.activity.endTime = moment();
+      			$scope.activity.timeActive = false;
+      		}
 		}
     }
   });
