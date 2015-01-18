2e771c556dc0f7dd40f51f7c3466dbc8cca5a89c
= <desmondclee@gmail.com>
2015-01-17 18:22:07 -0600

    persistence

--- app/entryScreen/entryScreen.js
+++ app/entryScreen/entryScreen.js
@@ -1,14 +1,34 @@
+var moment = require('moment')
+
 angular.module('entryScreen', [])
   .directive('entryScreen', function() {
     return {
     	templateUrl: 'entryScreen/entryScreen.html',
-    	controller: function($scope, $location) {
+    	controller: function($scope, $location, db) {
+        $scope.model = {};
   			$scope.activity = {
       			name: "",
       			duration: 0,
       			notes: ""
       		};
-      		$scope.test = 'hello world from test directive';
+        $scope.test = 'hello world from test directive';
+        $scope.submit = function() {
+          if (!$scope.model._id) {
+            $scope.model._id = new Date().toISOString()
+          }
+          if (!$scope.model.date) {
+            $scope.model.date = new Date().toISOString()
+          }
+          console.log($scope.model)
+          db.put($scope.model, function(err, result) {
+            if (!err) {
+              console.log("success")
+            }
+            else {
+              console.log(err)
+            }
+          })
+        }
 		}
     }
   });
