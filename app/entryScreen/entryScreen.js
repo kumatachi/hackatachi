b7d0fdb9f92a808ab00d16f6e9264cd96925f456
jamie crisman <jamie@awkwardhero.com>
2015-01-17 19:24:33 -0600

    saving to db

--- app/entryScreen/entryScreen.js
+++ app/entryScreen/entryScreen.js
@@ -3,9 +3,10 @@ angular.module('entryScreen', [])
   .directive('entryScreen', function() {
     return {
     	templateUrl: 'entryScreen/entryScreen.html',
-    	controller: function($scope, $location) {
+    	controller: function($scope, $location, db) {
     		$scope.timerActive = false;
   			$scope.activity = {
+      			_id: new Date().toISOString(),
       			name: "",
       			duration: 0,
       			notes: "",
@@ -22,7 +23,15 @@ angular.module('entryScreen', [])
       			$scope.timerActive = false;
       			var s = moment($scope.activity.startTime),
       				e = moment($scope.activity.endTime);
-      			$scope.activity.duration = e.diff(s).asMinutes(); //milli to minutes
+      			$scope.activity.duration = (e.diff(s)/1000)/60;//.asMinutes(); //milli to minutes
+      		}
+      		$scope.submit = function(){
+      			db.put($scope.activity, function(err, res) {
+  					if (err) {
+  						console.log(err);
+  					}
+  					console.log(res);
+      			});
       		}
 		}
     }
