2c46b7abe306d7c726657e209a14d10de94906fb
Thomas Grice <augustus.grice@gmail.com>
2015-01-17 20:57:25 -0600

    trying to add bar graph

--- app/progressPage/progressPage.js
+++ app/progressPage/progressPage.js
@@ -1,33 +1,49 @@
 angular.module('progressPage', [])
-  .directive('progressPage', function() {
-    return {
-      templateUrl: 'progressPage/progressPage.html',
-      controller: function($scope, dataService) {
-          $scope.dataset = [];
-          var dataProcess = function(){
-              dataService.getData().then(function(data) {
-                  data.forEach(function(datapoint){
-                      var tempObj = {
-                          label: datapoint.name,
-                          data: [datapoint.duration]
-                      };
-                      $scope.dataset.push(tempObj);
-                  })
-              });
-          };
+    .directive('progressPage', function () {
+        return {
+            templateUrl: 'progressPage/progressPage.html',
+            controller: function ($scope, dataService) {
+                $scope.pieShow = true;
+                $scope.barShow = false;
+                $scope.dataset = [];
+                var dataProcess = function () {
+                    dataService.getData().then(function (data) {
+                        data.forEach(function (datapoint) {
+                            var tempObj = {
+                                label: datapoint.name,
+                                data: [5, datapoint.duration],
+                                hoverable: true
+                            };
+                            $scope.dataset.push(tempObj);
+                        })
+                    });
+                };
 
-          $scope.options = {
-              series: {
-                  pie: {
-                      show: true
-                  }
-              },
-              legend: {
-                  show: false
-              }
-          };
+                $scope.pieOptions = {
+                    series: {
+                        pie: {
+                            show: true
+                        }
+                    },
+                    legend: {
+                        show: false
+                    }
+                };
 
-          dataProcess();
-      }
-    }
-  });
+
+                $scope.barOptions = {
+                    series: {
+                        bars: {
+                            barWidth: 5,
+                            align: "left"
+                        }
+                    },
+                    legend: {
+                        show: false
+                    }
+                };
+
+                dataProcess();
+            }
+        }
+    });
